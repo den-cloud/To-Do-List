4920c4b1488cceeed78935f599946c41eac65074
unknown <djpants90@yahoo.com>
2021-09-24 10:50:33 +0200

    Add new code to index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
+import { handleDragStart, handleDrop, cancelDefault } from './services/sort';
+import {
+  updateStatus, getTodos,
+} from './services/update';
 import './style.css';
 
-const element = document.getElementById('todos-list');
-
-const myTasks = [
-  { description: 'To buy veggies', index: 0, completed: false },
-  { description: 'To meet Erick', index: 1, completed: false },
-  { description: 'To complete Microverse Capstone', index: 2, completed: false },
-];
-
-const addList = (todosList) => {
-  for (let i = 0; i < todosList.length; i += 1) {
-    const listItem = ` <div class="d-flex justify-content-between ps-3 pe-3 align-items-center">
-        <div class="d-flex">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault${i}">
-        <div class="ps-3">${todosList[i].description}</div>
-        </div>
-        <i class="fas fa-ellipsis-v text-secondary" id=""></i>
-      </div>
-      <hr>`;
-
-    element.innerHTML += listItem;
+class TodoList {
+  constructor() {
+    this.todos = [
+      { description: 'Item 1', completed: false, index: 1 },
+      { description: 'Item 2', completed: false, index: 2 },
+      { description: 'Item 3', completed: false, index: 3 },
+    ];
   }
-};
 
-addList(myTasks);
+ handleCompletedStatus = (e) => {
+   const todos = getTodos();
+   updateStatus(e, todos);
+ }
+
+ displayList() {
+   const container = document.querySelector('.todoList');
+   this.todos.forEach((todo, index) => {
+     const li = document.createElement('li');
+     li.innerHTML = `
+                  <div class="task-item">
+                    <div class="task-info">
+                        <div class="check-container">
+                        <input id=todo-${todo.index} type="checkbox" ${todo.completed ? 'checked' : ''}>
+                        <label for="todo-${todo.index}" class="checkmark"></span>
+                        </div>
+                        <div class="input-container">
+                        <div>${todo.description}</div>
+                        <input data-index="${todo.index}"type="text" class="edit" value="${todo.description}" />
+                        </div>
+                    </div>
+                    <div class="icons">
+                      <i class="far fa-trash-alt todo-delete" data-index="${todo.index}"></i>
+                      <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                  </div>
+                `;
+     li.draggable = true;
+     li.dataset.index = index;
+     li.addEventListener('dragstart', handleDragStart);
+     li.addEventListener('drop', handleDrop);
+     li.addEventListener('dragenter', cancelDefault);
+     li.addEventListener('dragover', cancelDefault);
+     container.appendChild(li);
+   });
+ }
+}
+
+const todoList = new TodoList();
+todoList.displayList();
